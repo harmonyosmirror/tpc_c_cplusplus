aacbbe0aadf3eb8e530365bc9332838ecaf5a991
xiong-qiushi <xiong_123ing@163.com>
2024-06-20 18:58:26 +0800

    libyaml修改OAT文件
    Signed-off-by: xiong-qiushi <xiong_123ing@163.com>

--- thirdparty/libyaml/run-all-tests.sh
+++ thirdparty/libyaml/run-all-tests.sh
@@ -1,8 +1,44 @@
 #!/bin/sh
-ctest
+
+# Copyright (c) 2023 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Contributor: huangminzhong2 <huangminzhong2@huawei.com>
+# Maintainer: huangminzhong2 <huangminzhong2@huawei.com>
 
 total_tests=13
-passed_tests=2
+passed_tests=0
+
+./test-version
+res=$?
+if [ $res -ne 0 ]
+then
+    echo "1/13 test-version .......................... failed"
+else
+    echo "1/13 test-version .......................... passed"
+    passed_tests=`expr $passed_tests + 1`
+fi
+
+./test-reader
+res=$?
+if [ $res -ne 0 ]
+then
+    echo "2/13 test-reader .......................... failed"
+else
+    echo "2/13 test-reader .......................... passed"
+    passed_tests=`expr $passed_tests + 1`
+fi
+
 cat ../examples/anchors.yaml | ./example-deconstructor
 res=$?
 if [ $res -ne 0 ]
